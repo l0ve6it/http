e8edd6c09c45d93e5510389111c4d60eaa3b4f39
l0ve6it <1850289475@qq.com>
2017-04-17 16:23:03 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-document.write("<script language=javascript src='https://hm.baidu.com/hm.js?527b7edb5c1d51d635b72fd2f8c0866f'></script>”);
+document.write("<script language=javascript src='https://hm.baidu.com/hm.js?527b7edb5c1d51d635b72fd2f8c0866f'></script>");
