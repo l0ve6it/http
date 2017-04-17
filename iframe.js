fc675ca8c92a150bf782f8d839973d1536a3dc55
l0ve6it <1850289475@qq.com>
2017-04-17 08:41:41 +0800

    Create iframe.js

---
+++ iframe.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?527b7edb5c1d51d635b72fd2f8c0866f";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
