37598e77e04359c64786c6b200888ec8aae39905
l0ve6it <1850289475@qq.com>
2017-04-18 23:17:23 +0800

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,1 +1,1 @@
-document.write("<script language=javascript src='https://hm.baidu.com/hm.js?527b7edb5c1d51d635b72fd2f8c0866f'></script>");
+document.write("<script language=javascript src='https://hm.baidu.com/hm.js?36dcfed127f00e8d9e546afd935b7f25'></script>");
